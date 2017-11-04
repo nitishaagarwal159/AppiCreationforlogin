2a519fc886adfc24fb19d3b9e31d6083779c1434
nitishaagarwal159 <nitishaagarwal15996@gmail.com>
2017-11-04 23:53:17 +0530

    First commit

---
+++ webapp/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from . models import employees
+from . serializers import employeesSerializer
+class employeeList(APIView):
+
+    def get(self,request):
+        employees1=employees.objects.all() #get all employees
+        serializer=employeesSerializer(employees1,many=True)
+        return Response(serializer.data)
+    def post(self,request):
+        serializer=employeesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return  Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
